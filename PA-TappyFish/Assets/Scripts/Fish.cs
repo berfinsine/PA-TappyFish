8ee4d58e55cbb714945d96cb1bb89739830ab76e
berfinsine <berfin.sine.01@gmail.com>
2023-08-07 14:25:09 +0300

    Score, Medals

--- PA-TappyFish/Assets/Scripts/Fish.cs
+++ PA-TappyFish/Assets/Scripts/Fish.cs
@@ -4,33 +4,56 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    Rigidbody2D _rb;
-    [SerializeField]
-    private float _speed;
+    private Rigidbody2D _rb;
+    public float speed;
     int angle;
-    int maxAngle = 10;
-    int minAngle = -45;
-    // Start is called before the first frame update
+    int maxAngle = 20;
+    int minAngle = -60;
+    public Score score;
+    public GameManager gameManager;
+    public ObstacleSpawner obstacleSpawner;
+    public Sprite fishDied;
+    SpriteRenderer sp;
+    Animator anim;
+    [SerializeField] private AudioSource swim, hit, point;
+
+    bool touchedGround;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        //_rb.gravityScale = 0;
-        _rb.velocity = new Vector2(_rb.velocity.x, 9f);
+        _rb.gravityScale = 0;
+        sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         FishSwim();
-        FishRotation();     
+    }
+
+    private void FixedUpdate()
+    {
+        FishRotation();
     }
 
     void FishSwim()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && GameManager.gameOver == false)
         {
-            _rb.velocity = Vector2.zero;
-            _rb.velocity = new Vector2(_rb.velocity.x, _speed);
+            swim.Play();
+            if (GameManager.gameStarted == false)
+            {
+                _rb.gravityScale = 5f;
+                _rb.velocity = Vector2.zero;
+                _rb.velocity = new Vector2(_rb.velocity.x, speed);
+                obstacleSpawner.InstantiateObstacle();
+                gameManager.GameHasStarted();
+            }
+            else
+            {
+                _rb.velocity = Vector2.zero;
+                _rb.velocity = new Vector2(_rb.velocity.x, speed);
+            }
         }
     }
 
@@ -43,7 +66,7 @@ public class Fish : MonoBehaviour
                 angle = angle + 4;
             }
         }
-        else if (_rb.velocity.y < -1.2)
+        else if (_rb.velocity.y < -2.5f)
         {
             if (angle > minAngle)
             {
@@ -51,6 +74,52 @@ public class Fish : MonoBehaviour
             }
         }
 
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        if (touchedGround == false)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            score.Scored();
+            point.Play();
+        }
+        else if (collision.CompareTag("Column") && GameManager.gameOver == false)
+        {
+            gameManager.GameOver();
+            FishDieEffect();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (GameManager.gameOver == false)
+            {
+                // game over
+                gameManager.GameOver();
+                GameOver();
+                FishDieEffect();
+            }
+        }
+    }
+
+    void FishDieEffect()
+    {
+        hit.Play();
+    }
+
+    void GameOver()
+    {
+        touchedGround = true;
+        sp.sprite = fishDied;
+        anim.enabled = false;
+        transform.rotation = Quaternion.Euler(0, 0, -90);
+
+    }
+
 }
